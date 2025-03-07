512fa122eb7bcf42d75c47e8b5dd67c1af1e4bf3
useryesa9@gmail.com <useryesa9@gmail.com>
2025-03-08 05:38:13 +0700

    membuat fungsi memperbarui

--- main.go
+++ main.go
@@ -77,11 +77,53 @@ func lihatDetail(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func perbarui(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPut {
+		http.Error(w, "Hanya mendukung method Put", http.StatusMethodNotAllowed)
+		return
+	}
+
+	idStr := strings.TrimPrefix(r.URL.Path, "/todo/perbarui/")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "ID tidak valid!", http.StatusBadRequest)
+		return
+	}
+
+	var updateTodo Todo
+	err = json.NewDecoder(r.Body).Decode(&updateTodo)
+	if err != nil {
+		http.Error(w, "Gagal decode JSON", http.StatusBadRequest)
+		return
+	}
+
+	for i, t := range todo_list {
+		if t.ID == id {
+			todo_list[i].Judul = updateTodo.Judul
+			todo_list[i].Deskripsi = updateTodo.Deskripsi
+			todo_list[i].Selesai = updateTodo.Selesai
+
+			response := map[string]interface{}{
+				"pesan":  "Berhasil memperbarui data!",
+				"data":   todo_list[i],
+				"status": http.StatusOK,
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(response)
+			return
+		}
+	}
+
+	http.Error(w, "Todo tidak ditemukan", http.StatusNotFound)
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/todo/tambah", tambah)
 	mux.HandleFunc("/todo/lihat-semua", lihatSemua)
 	mux.HandleFunc("/todo/lihat-detail/{id}", lihatDetail)
+	mux.HandleFunc("/todo/perbarui/{id}", perbarui)
 
 	http.ListenAndServe(":99", mux)
 }
